2027346d7ceb8fb3485ee7f4b86eb4e9368d410b
ngv09 <113272579+ngv09@users.noreply.github.com>
2026-07-08 11:26:51 -0500

    write possession helper

--- src/metrics.py
+++ src/metrics.py
@@ -35,3 +35,75 @@ def empty_metric(metric_name:str) -> pd.DataFrame:
     return pd.DataFrame(
         columns=GROUP_COLS + [metric_name]
     )
+
+def compute_team_match_possession(events:pd.DataFrame) -> pd.DataFrame:
+    """
+    Team possession by match.
+    
+    possession = 
+    team_passes /
+    (team_passes + opponent_passes)
+    """
+
+    validate_columns(
+        events,
+        ["match_id", "team", "type"],
+        "compute_team_match_possession"
+    )
+
+    passes = events.loc[
+        events["type"] == "Pass"
+    ]
+
+    team_passes = (
+        passes.groupby(["match_id", "team"])
+        .size()
+        .reset_index(name="team_passes")
+    )
+
+    possession_rows = []
+
+    for match_id, match_df in team_passes.groupby("match_id"):
+
+        if len(match_df) != 2: 
+            continue
+
+        row_a = match_df.iloc[0]
+        row_b = match_df.iloc[1]
+
+        possession_rows.append(
+            {
+                "match_id": match_id,
+                "team": row_a["team"],
+                "team_passes": row_a["team_passes"],
+                "opponent_passes": row_b["team_passes"],
+            }
+        )
+
+        possession_rows.append(
+            {
+                "match_id": match_id,
+                "team": row_b["team"],
+                "team_passes": row_b["team_passes"],
+                "opponent_passes": row_a["team_passes"],
+            }
+        )
+
+        possession = pd.DataFrame(possession_rows)
+
+        possession["possession"] = (
+            possession["team_passes"] /
+            (
+                possession["team_passes"] + possession["opponent_passes"]
+            )
+        )
+
+        possession["adjustment_factor"] = (
+            2 /
+            (
+                1 + np.exp(-10 * (possession["possession"] - 0.5))
+            )
+        )
+
+        return possession
+    

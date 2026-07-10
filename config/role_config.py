69ac5d9e751ffd0a44b757e7c5261d24690ece1e
ngv09 <113272579+ngv09@users.noreply.github.com>
2026-07-10 13:25:15 -0500

    add role configuration

---
+++ config/role_config.py
@@ -0,0 +1,47 @@
+ROLE_METRICS = {
+    "Full Back": [
+        "deep_progressions_per90",
+        "open_play_xg_assisted_per90",
+        "turnovers_per90",
+        "aerial_win_pct",
+        "padj_tackles_interceptions_per90",
+        "padj_pressures_per90",
+        "fouls_per90",
+        "tackle_dribbled_past_pct",
+    ],
+    "Defensive Midfielder": [
+        "deep_progressions_per90",
+        "open_play_xg_assisted_per90",
+        "padj_tackles_interceptions_per90",
+        "tackle_dribbled_past_pct",
+        "padj_pressures_per90",
+        "fouls_per90",
+        "turnovers_per90",
+    ],
+    "Creative Midfielder": [
+        "touches_in_box_per90",
+        "open_play_xg_assisted_per90",
+        "shots_per90",
+        "npxg_per90",
+        "npxg_per_shot",
+        "padj_pressures_per90",
+        "fouls_won_per90",
+        "turnovers_per90",
+    ],
+}
+
+DISPLAY_NAMES = {
+    "deep_progressions_per90": "Deep Progressions /90",
+    "open_play_xg_assisted_per90": "Open Play xG Assisted /90",
+    "turnovers_per90": "Turnovers /90",
+    "aerial_win_pct": "Aerial Win %",
+    "padj_tackles_interceptions_per90": "PAdj Tackles + Interceptions /90",
+    "padj_pressures_per90": "PAdj Pressures /90",
+    "fouls_per90": "Fouls /90",
+    "tackle_dribbled_past_pct": "Tackle / Dribbled Past %",
+    "touches_in_box_per90": "Touches In Box /90",
+    "shots_per90": "Shots /90",
+    "npxg_per90": "NPxG /90",
+    "npxg_per_shot": "NPxG / Shot",
+    "fouls_won_per90": "Fouls Won /90",
+}
